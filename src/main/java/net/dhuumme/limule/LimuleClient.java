9c3b6114d7e6f1d840f1c3824826f9a237de9ad5
DhuumMe <benoit.abelard@gmail.com>
2024-08-15 22:25:30 +0200

    Initial commit

---
+++ src/main/java/net/dhuumme/limule/LimuleClient.java
@@ -0,0 +1,10 @@
+package net.dhuumme.limule;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LimuleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
